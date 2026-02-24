0ccadb146bf79f06fae3cd2f5b3ade863d4617a9
nabdurahim-art <n.abdurahim@alustudent.com>
2026-02-24 06:30:09 -0800

    import and modules scripts

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add as add_0
-
 if __name__ == "__main__":
+    from add_0 import add
+
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add_0(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
