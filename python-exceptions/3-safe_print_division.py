fd504079f71bcd624c2da6c6dda946c9f7cc9116
nabdurahim-art <n.abdurahim@alustudent.com>
2026-03-08 05:06:20 -0700

    function that prints all integers of a list, in reverse order

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-def safe_print_division(a, b):
-    result = None
-    try:
-        result = a / b
-    except ZeroDivisionError:
-        result = None
-    finally:
-        print("Inside result: {}".format(result))
-    return result
+def print_reversed_list_integer(my_list=[]):
+    if my_list:
+        for i in reversed(my_list):
+            print("{:d}".format(i))
